e4a3c087785e5c925cc21646329a6dc0810cf22b
Ramesh T <ramesht@srmtech.com>
2019-08-28 13:07:31 +0530

    Updates

---
+++ precache-manifest.445d1bf2ba937a600cfa1e043e20047b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0ea77cf0c8c52e31c9a4eb1b03c0f94",
+    "url": "/react-redux-pagination/index.html"
+  },
+  {
+    "revision": "692c9d8b7572866be29b",
+    "url": "/react-redux-pagination/static/css/main.bdca4a30.chunk.css"
+  },
+  {
+    "revision": "29fc6dfa61f7038de222",
+    "url": "/react-redux-pagination/static/js/2.ecb42d2c.chunk.js"
+  },
+  {
+    "revision": "692c9d8b7572866be29b",
+    "url": "/react-redux-pagination/static/js/main.eb2038e6.chunk.js"
+  },
+  {
+    "revision": "3ebab7b7d06337a5a8c3",
+    "url": "/react-redux-pagination/static/js/runtime~main.f7fc4cbe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-redux-pagination/static/media/logo.5d5d9eef.svg"
+  }
+]);
